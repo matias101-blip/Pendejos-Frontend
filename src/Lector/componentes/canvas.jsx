2f0e70f23372148420d9f6118e5624ccba43eb3b
Thehunter101 <thehunter10121@gmail.com>
2025-02-24 16:20:43 -0500

    lazy complete

--- src/Lector/componentes/canvas.jsx
+++ src/Lector/componentes/canvas.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState, createContext, useContext } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { Layer, Stage, Image as KonvaImage } from 'react-konva';
 import { isMobile } from 'react-device-detect';
 import useImage from 'use-image';
-
-const CtxHojas = createContext();
-
+import { Box, Spinner, Container, Center } from '@chakra-ui/react';
 const Hoja = (props) => {
-  const { index, capitulo, name, pagina, window, PoscY, onLoad } = props;
+  const { index, capitulo, name, pagina, window } = props;
   const [posX, setPosX] = useState(0);
-  const { sizeWindow, setSizeWindow } = useContext(CtxHojas);
 
   const [Image] = useImage(
     `https://pendejosapi.space/img/${name}/${capitulo}/${pagina}-webp`,
@@ -18,80 +16,81 @@ const Hoja = (props) => {
       const ImageWOrg = Image.width;
       const ImageHOrg = Image.height;
       const hRatio = window.w / ImageWOrg;
-      const vRatio = 1080 / ImageHOrg;
+      const vRatio = window.h / ImageHOrg;
 
+      console.log(window.h);
       const ratio = Math.min(hRatio, vRatio);
       const newWidth = ImageWOrg * ratio;
       const newHeight = ImageHOrg * ratio;
 
       Image.width = newWidth;
       Image.height = newHeight;
+      console.log(`${Image.width}:${Image.height}`);
 
       setPosX((window.w - newWidth) / 2);
-
-      setSizeWindow((prev) => ({
-        ...prev,
-        h: prev.h + newHeight,
-      }));
-
-      onLoad(newHeight); // Pasa la altura de la imagen al callback
     }
   }, [Image]);
 
-  return <KonvaImage image={Image} x={posX} y={PoscY} />;
+  return <KonvaImage image={Image} x={posX} />;
 };
 
-const Canvas = (props, { children }) => {
+const Canvas = (props) => {
   const hojas = props.hojas;
   const name = props.nombre;
   const capitulo = props.capitulo;
   const [loadImages, setLoadImages] = useState(0);
   const [imageHeights, setImageHeights] = useState([]);
   const [pags, setPags] = useState([]);
-
+  const [indexPag, setIndexPag] = useState(0);
   const [sizeWindow, setSizeWindow] = useState({
     w: window.innerWidth,
     h: 0,
   });
 
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
+
   useEffect(() => {
-    if (!isMobile) {
-      setSizeWindow((prev) => ({
-        ...prev,
-        w: 1024,
-      }));
+    if (inView) {
+      addPage();
     }
-  }, []);
+  }, [inView]);
+
   const addPage = () => {
     setPags((prevPags) => [
       ...prevPags,
-      <Hoja
-        id={prevPags.length + 1}
-        key={prevPags.length}
-        capitulo={3}
-        name={name}
-        pagina={1}
-        window={{ w: sizeWindow.w, h: sizeWindow.h }}
-        PoscY={imageHeights}
-        onLoad={(height) => {
-          setImageHeights((prev) => [...prev, height]); // Guarda la altura de la imagen
-          setLoadImages((prev) => prev + 1);
-        }}
-      />,
+      {
+        id: `pags-${indexPag}`,
+        pagina: indexPag,
+      },
     ]);
+    setIndexPag(indexPag + 1);
   };
+
   return (
     <div>
-      <Stage width={sizeWindow.w} height={sizeWindow.h}>
-        <Layer listening={false}>
-          <CtxHojas.Provider
-            value={{ sizeWindow, setSizeWindow, setImageHeights }}
-          >
-            {pags}
-          </CtxHojas.Provider>
-        </Layer>
-      </Stage>
-      <button onClick={addPage}>Dame click!!</button>
+      {pags.map((pag) => (
+        <Stage width={720} height={1080}>
+          <Layer listening={false}>
+            <Hoja
+              capitulo={capitulo}
+              name={name}
+              pagina={pag.pagina}
+              window={{ w: 720, h: 1080 }}
+            />
+          </Layer>
+        </Stage>
+      ))}
+      <Container
+        alignContent="center"
+        ref={ref}
+        hidden={indexPag == hojas.length}
+      >
+        <Center>
+          <Spinner size="xl" />
+        </Center>
+      </Container>
     </div>
   );
 };
