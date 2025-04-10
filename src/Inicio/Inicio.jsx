ae063cc07be7c0b627646dd9a3cf7a8c58857526
Thehunter101 <thehunter10121@gmail.com>
2025-04-10 00:21:08 -0500

    updateInfo

--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -18,7 +18,7 @@ function Inicio() {
   return (
     <Box className="container">
       <Stack className="Seccion1" spacing="8" align="center">
-        <Heading>Lo mas popular Unu:</Heading>
+        <Heading>Escanor es gei Unu</Heading>
         <HStack
           mb="1.2em"
           spacing="0"
